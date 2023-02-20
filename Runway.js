b7bb212eb6db925c08b18335f3ff4462dcd1d5a5
Doreen Kamushinda <dkamushinda@expediagroup.com>
2023-02-20 16:43:53 +0000

    remove method added

--- Runway.js
+++ Runway.js
@@ -1,18 +1,24 @@
 class Runway {
-    static MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS = 100;
-    static planes = [];
+  static MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS = 100;
+  static planes = [];
 
-    name;
+  name;
 
-    constructor(name) {
-        this.name = name
+  constructor(name) {
+    this.name = name;
+  }
+
+  add(plane) {
+    if (Runway.planes.length > Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
+      throw new Error('runways at full capacity!');
     }
-    
-    add(plane) {
+    Runway.planes.push(plane);
+  }
 
-        if (Runway.planes.length>Runway.MAX_PLANES_ALLOWED_ON_ALL_RUNWAYS) {
-            throw new Error ("runways at full capacity!")
-        }
-        Runway.planes.push(plane)
+  remove(plane) {
+    const index = Runway.planes.indexOf(plane);
+    if (index > -1) {
+      Runway.planes.splice(index, 1);
     }
+  }
 }
